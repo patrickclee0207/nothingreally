a8309cdd406af66ee6149bacd43df3c405d35e41
Patrick Lee <patrickclee@hotmail.com>
2012-07-22 19:27:25 -0500

    created repo
    created the repo and added a simple wpf app that i was showing the kids
    some basic stuff

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        int x = 0;
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+            String[] phrase = { "you are STINKY", "you are TROUBLE", "you are a PAIN", "you are a GROWDALOPE" };
+            if (x == phrase.Count())
+            {
+                x = 0;
+                this.txtblock.Text = "Hello " + this.txtbox.Text + ", " + phrase[x].ToString();
+            }
+            else
+            {
+                this.txtblock.Text = "Hello " + this.txtbox.Text + ", " + phrase[x].ToString();
+                x++;
+            }
+        }
+    }
+}
